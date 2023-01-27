be732eeefa7ffd59af530c1cc016d724d61269a3
DannyQuintana <119816419+DannyQuintana@users.noreply.github.com>
2023-01-26 16:34:32 -0800

    Moved package locations

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,8 @@ module com.scheduleapp.c195task1 {
     requires java.sql;
 
 
-    exports com.scheduleapp.c195task1.controller;
-    opens com.scheduleapp.c195task1.controller to javafx.fxml;
+    exports controller;
+    opens controller to javafx.fxml;
     exports com.scheduleapp.c195task1;
     opens com.scheduleapp.c195task1 to javafx.fxml;
 }
